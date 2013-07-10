dc285387057c9cd2f052aa880e0af1c240cda181
AlisaEx <eatliightning@gmail.com>
2013-07-10 12:01:34 -0400

    finished chapter 4

--- examples.js
+++ examples.js
@@ -65,3 +65,100 @@ function greaterThan(x){
 	};
 };
 
+// Example 4.1
+cats = {"Zooey": true};
+cats["Baby"] = true;
+detele cats["noneofmine"];
+show("Zooey" in cats);
+
+// Example 4.2
+function range(num){
+	var something = [];
+	for (i=0; i<=num; i++){
+		something.push(i);
+	}
+	return something;
+}
+
+// Example 4.3
+thing = ["a","b","c d"];
+show(thing.join(" ").split(" "));
+
+// Example 4.4
+function startsWith(first, second){
+	return first.slice(0, second.length) === second;
+}
+
+// Example 4.5
+function catNames(p){
+	location = p.indexOf(":");
+	return p.slice(location+2).split(",");
+}
+
+// Example 4.6
+function extractDate(p){
+	function dateAt(start, end)
+		return Number(p.slice(start,start+end));
+	}
+	return new Date(dateAt(11,4), dateAt(8,2)-1, dateAt(5,2));
+};
+
+// Example 4.7
+function between(p, start, end){
+	var startAt = p.indexOf(start)+start.length;
+	var endAt = p.indexOf(end,startAt);
+	return p.slice(startAt, endAt);
+}
+
+// Example 4.8
+function formatDate(date){
+	function addZero(number){
+		if (number < 10){
+			return "0"+number;
+		}
+		else{
+			return number;
+		}
+	}
+  return pad(date.getDate() + "/" + (date.getMonth() + 1) +
+         "/" + date.getFullYear());
+}
+
+// Example 4.9
+function oldestCat(obj){
+	var oldest = null;
+	for (name in obj){
+		var cat = obj[name];
+		if(!(death in cat) && (oldest==null || oldest.birth >  cat.birth)){
+			oldest = cat;
+		}
+	}
+	if (oldest == null){
+		return "ALL THE CATS ARE DEAD!";
+	}
+	else{
+		return oldest.name;
+	}
+}
+
+// Example 4.10
+function range(begin, stop){
+	var something = [];
+	if(arguments.length < 2){
+		stop = begin;
+		begin = 0;
+	}
+	for(i=begin, i<=stop, i++){
+		something.push(i);
+	}
+	return something;
+}
+
+// Example 4.11
+function sum(array){
+	result = 0;
+	for (i = 0; i < array.length; i++){
+		result += array[i];
+	}
+	return result;
+}
