f9801a588ce951d5f829511b2033c35bbb245761
AlisaEx <eatliightning@gmail.com>
2013-07-10 17:43:50 -0400

    started chapter 6

--- examples.js
+++ examples.js
@@ -162,3 +162,79 @@ function sum(array){
 	}
 	return result;
 }
+
+// Example 6.1
+function countZeros(numbers){
+	function count(total, amount){
+		total + (amount===0 ? 1:0)
+	return reduce(count, 0, numbers);
+	}
+}
+
+function count(array, test){
+	reduce(function(total, element){
+		return total + (test(element) ? 1:0)
+	}, 0, array);
+}
+
+function equals(x) {
+  return function(element) {return x === element;};
+}
+
+function countZeroes(array) {
+  return count(array, equals(0));
+}
+
+// Example 6.2
+function processParagraph(p){
+	var header = 0;
+	while(p.charAt(0)===%){
+		p.slice(1);
+		header += 1;
+	}
+	return {type: (header===0 ? "p" : "h"+ header), content:p};
+}
+
+// Example 6.3  STILL NEED EXPLAINGING
+function splitParagraph(text) {
+  function indexOrEnd(character) {
+    var index = text.indexOf(character);
+    return index == -1 ? text.length : index;
+  }
+
+  function takeNormal() {
+    var end = reduce(Math.min, text.length,
+                     map(indexOrEnd, ["*", "{"]));
+    var part = text.slice(0, end);
+    text = text.slice(end);
+    return part;
+  }
+
+  function takeUpTo(character) {
+    var end = text.indexOf(character, 1);
+    if (end == -1)
+      throw new Error("Missing closing '" + character + "'");
+    var part = text.slice(1, end);
+    text = text.slice(end + 1);
+    return part;
+  }
+
+  var fragments = [];
+
+  while (text != "") {
+    if (text.charAt(0) == "*")
+      fragments.push({type: "emphasised",
+                      content: takeUpTo("*")});
+    else if (text.charAt(0) == "{")
+      fragments.push({type: "footnote",
+                      content: takeUpTo("}")});
+    else
+      fragments.push({type: "normal",
+                      content: takeNormal()});
+  }
+  return fragments;
+}
+// Example 6.4
+function createImage(location){
+	return tag("img", [], {src: location});
+}
