5496e7c5d2e79ea7d2e6d9d0dfafdb194675a691
AlisaEx <eatliightning@gmail.com>
2013-07-17 14:40:44 -0400

    finished Chapter 13

--- examples.js
+++ examples.js
@@ -542,3 +542,42 @@ userForm.elements.send.onclick = function() {
     	alert("Enter your shit in right dog!");
 	}
 };
+
+	/// Example 12.1 ///
+function asHTML(node) {
+	if (isTextNode(node)){
+		return escapeHTML(node.nodeValue);
+	}
+  	else if (node.childNodes.length == 0){
+    	return "<" + node.nodeName + "/>";
+	}
+  	else{
+    	return "<" + node.nodeName + ">" +
+           map(asHTML, node.childNodes).join("") +
+           "</" + node.nodeName + ">";
+    }
+}
+
+	/// Example 12.2 ///
+function removeElement(node) {
+  	if (node.parentNode){
+    	node.parentNode.removeChild(node);
+	}
+}
+
+	/// Example 12.3 ///
+function makeTable(data, columns) {
+  	var headRow = dom("TR");
+  	forEach(columns, function(name) {
+    	headRow.appendChild(dom("TH", null, name));
+  	});
+  	var body = dom("TBODY", null, headRow);
+  	forEach(data, function(object) {
+    	var row = dom("TR");
+    	forEach(columns, function(name) {
+      		row.appendChild(dom("TD", null, String(object[name])));
+    	});
+    	body.appendChild(row);
+  	});
+  	return dom("TABLE", null, body);
+}
